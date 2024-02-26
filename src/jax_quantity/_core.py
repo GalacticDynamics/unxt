0ac82758c75f347919335e11fdb8d83caadadb01
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-02-25 23:08:50 -0500

    feat: unit and dtype kwargs to constructor (#38)
    * feat: unit and dtype kwargs to constructor
    * fix: dtype import
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/jax_quantity/_core.py
+++ src/jax_quantity/_core.py
@@ -22,7 +22,7 @@ from astropy.units import (
     UnitConversionError,
     get_physical_type,
 )
-from jax.numpy import dtype
+from jax.numpy import dtype as DType  # noqa: N812
 from jaxtyping import Array, ArrayLike, Shaped
 from plum import conversion_method, parametric
 from quax import ArrayValue, quaxify
@@ -201,7 +201,7 @@ class Quantity(ArrayValue):  # type: ignore[misc]
     __rtruediv__ = quaxify(_flip_binop(operator.truediv))
 
     @property
-    def dtype(self) -> dtype:
+    def dtype(self) -> DType:
         """Data type of the array."""
         return self.value.dtype
 
@@ -283,9 +283,17 @@ def constructor(cls: type[Quantity], value: Quantity, unit: Any, /) -> Quantity:
 
 
 @Quantity.constructor._f.register  # type: ignore[no-redef] # noqa: SLF001
-def constructor(cls: type[Quantity], value: Quantity, /) -> Quantity:
+def constructor(
+    cls: type[Quantity],
+    value: Quantity,
+    /,
+    *,
+    unit: Any | None = None,
+    dtype: Any = None,
+) -> Quantity:
     """Construct a `Quantity` from another `Quantity`, with no unit change."""
-    return replace(value)
+    unit = value.unit if unit is None else unit
+    return xp.asarray(value.to(unit), dtype=dtype)
 
 
 @Quantity.constructor._f.register  # type: ignore[no-redef] # noqa: SLF001
