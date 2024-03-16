451beb64e121a34188817c61cee0f354c6d0607b
nstarman <nstarman@users.noreply.github.com>
2024-03-16 11:22:53 -0400

    feat: compat
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

---
+++ src/unxt/_compat.py
@@ -0,0 +1,31 @@
+"""Compatibility layer for `astropy.units`."""
+
+__all__: list[str] = []
+
+
+from astropy.units import Quantity as AstropyQuantity
+from plum import conversion_method
+
+from ._base import AbstractQuantity
+from ._core import Quantity
+from ._fast import UncheckedQuantity
+
+
+@conversion_method(type_from=AbstractQuantity, type_to=AstropyQuantity)  # type: ignore[misc]
+def convert_quantity_to_astropyquantity(obj: AbstractQuantity, /) -> AstropyQuantity:
+    """`AbstractQuantity` -> `astropy.AbstractQuantity`."""
+    return AstropyQuantity(obj.value, obj.unit)
+
+
+@conversion_method(type_from=AstropyQuantity, type_to=UncheckedQuantity)  # type: ignore[misc]
+def convert_astropyquantity_to_uncheckedquantity(
+    obj: AstropyQuantity, /
+) -> UncheckedQuantity:
+    """`astropy.AbstractQuantity` -> `UncheckedQuantity`."""
+    return UncheckedQuantity(obj.value, obj.unit)
+
+
+@conversion_method(type_from=AstropyQuantity, type_to=Quantity)  # type: ignore[misc]
+def convert_astropyquantity_to_quantity(obj: AstropyQuantity, /) -> Quantity:
+    """`astropy.AbstractQuantity` -> `Quantity`."""
+    return Quantity(obj.value, obj.unit)
