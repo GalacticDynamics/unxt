f779c4dbc14b447782130fe9326f3028ec8c4d7c
nstarman <nstarman@users.noreply.github.com>
2024-03-27 15:06:48 -0400

    refactor: constant type
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_distance.py
+++ src/unxt/_distance.py
@@ -5,24 +5,31 @@ __all__ = ["Distance"]
 
 from typing import TypeVar, final
 
-from plum import parametric
+import astropy.units as u
 
 import quaxed.numpy as qnp
 
-from ._core import AbstractParametricQuantity, Quantity
+from ._base import AbstractQuantity
+from ._core import Quantity
 
 FMT = TypeVar("FMT")
 
 parallax_base_length = Quantity(1, "AU")
+length_dimension = u.get_physical_type("length")
 
 
 ##############################################################################
 
 
 @final
-@parametric
-class Distance(AbstractParametricQuantity):
+class Distance(AbstractQuantity):
     """Distance quantities."""
+
+    def __check_init__(self) -> None:
+        """Check the initialization."""
+        if self.unit.physical_type != length_dimension:
+            msg = "Distance must have dimensions length."
+            raise ValueError(msg)
 
     @property
     def parallax(  # noqa: PLR0206  (needed for quax boundary)
