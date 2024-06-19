30795fb912725b39d272b5e3296b9cf390d334dd
nstarman <nstarman@users.noreply.github.com>
2024-06-19 13:21:18 -0400

    docs: skip tests in Windows until fixed numpy 2.0+

--- conftest.py
+++ conftest.py
@@ -1,17 +1,24 @@
 """Doctest configuration."""
 
-from __future__ import annotations
-
+import platform
 from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
 
 from sybil import Sybil
 from sybil.parsers.rest import DocTestParser, PythonCodeBlockParser, SkipParser
 
+# TODO: stop skipping doctests on Windows when there is uniform support for
+#       numpy 2.0+ scalar repr. On windows it is printed as 1.0 instead of
+#       `np.float64(1.0)`.
+parsers = (
+    [DocTestParser(optionflags=ELLIPSIS | NORMALIZE_WHITESPACE)]
+    if platform.system() != "Windows"
+    else []
+) + [
+    PythonCodeBlockParser(),
+    SkipParser(),
+]
+
 pytest_collect_file = Sybil(
-    parsers=[
-        DocTestParser(optionflags=ELLIPSIS | NORMALIZE_WHITESPACE),
-        PythonCodeBlockParser(),
-        SkipParser(),
-    ],
+    parsers=parsers,
     patterns=["*.rst", "*.py"],
 ).pytest()
