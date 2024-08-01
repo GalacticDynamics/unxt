e113ffce46ef771e2dee423f34fa8814327e22cf
nstarman <nstarman@users.noreply.github.com>
2024-07-31 23:31:14 -0400

    style: return types
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_quantity/base.py
+++ src/unxt/_quantity/base.py
@@ -5,7 +5,7 @@ __all__ = ["AbstractQuantity", "can_convert_unit"]
 
 from collections.abc import Callable, Mapping, Sequence
 from dataclasses import fields, replace
-from typing import TYPE_CHECKING, Any, ClassVar, TypeAlias, TypeGuard, TypeVar
+from typing import TYPE_CHECKING, Any, ClassVar, NoReturn, TypeAlias, TypeGuard, TypeVar
 from typing_extensions import Self
 
 import equinox as eqx
@@ -277,7 +277,7 @@ class AbstractQuantity(ArrayValue):  # type: ignore[misc]
         """Shape of the array."""
         return self.value.shape
 
-    def materialise(self) -> None:
+    def materialise(self) -> NoReturn:
         msg = "Refusing to materialise `Quantity`."
         raise RuntimeError(msg)
 
@@ -313,7 +313,7 @@ class AbstractQuantity(ArrayValue):  # type: ignore[misc]
         """
         return replace(self, value=self.to_units_value(units), unit=units)
 
-    def to_units_value(self, units: Unit) -> ArrayLike:
+    def to_units_value(self, units: Unit) -> Array:
         """Return the value in the given units.
 
         Parameters
