38dfe907bb8300d006653c0f33cd6a06af82f02c
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-08-30 15:18:48 -0400

    refactor: singleton mixin (#166)
    * refactor: singleton mixin
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_unxt/unitsystems/builtin.py
+++ src/unxt/_unxt/unitsystems/builtin.py
@@ -13,27 +13,29 @@ from astropy.units import dimensionless_unscaled
 
 from . import builtin_dimensions as ud  # noqa: TCH001
 from .base import AbstractUnitSystem
+from unxt._unxt.utils import SingletonMixin
 
 Unit: TypeAlias = u.UnitBase
-
-_dimless_insts: dict[type[DimensionlessUnitSystem], DimensionlessUnitSystem] = {}
 
 
 @final
 @dataclass(frozen=True, slots=True)
-class DimensionlessUnitSystem(AbstractUnitSystem):
-    """A unit system with only dimensionless units."""
+class DimensionlessUnitSystem(SingletonMixin, AbstractUnitSystem):
+    """A unit system with only dimensionless units.
+
+    This is a singleton class.
+
+    Examples
+    --------
+    >>> from unxt.unitsystems import DimensionlessUnitSystem
+    >>> dims1 = DimensionlessUnitSystem()
+    >>> dims2 = DimensionlessUnitSystem()
+    >>> dims1 is dims2
+    True
+
+    """
 
     dimensionless: Annotated[Unit, ud.dimensionless] = dimensionless_unscaled
-
-    def __new__(cls) -> DimensionlessUnitSystem:
-        # Check if instance already exists
-        if cls in _dimless_insts:
-            return _dimless_insts[cls]
-        # Create new instance and cache it
-        self = object.__new__(cls)
-        _dimless_insts[cls] = self
-        return self
 
     def __repr__(self) -> str:
         return "DimensionlessUnitSystem()"
