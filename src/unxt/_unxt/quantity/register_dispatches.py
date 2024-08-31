cbd05094ff5fceac745fb519015b398516566a8a
nstarman <nstarman@users.noreply.github.com>
2024-08-31 14:55:48 -0400

    docs: add for dispatches
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_unxt/quantity/register_dispatches.py
+++ src/unxt/_unxt/quantity/register_dispatches.py
@@ -6,7 +6,7 @@ from typing import Any, TypeVar
 import jax
 import jax.core
 import jax.experimental.array_api as jax_xp
-import numpy as np
+from jaxtyping import ArrayLike
 from plum import Dispatcher, Function
 from plum.parametric import type_unparametrized as type_np
 
@@ -35,13 +35,37 @@ def chain_dispatchers(*dispatchers: Dispatcher) -> Callable[[Any], Function]:
 
 @chain_dispatchers(np_dispatcher, xp_dispatcher)
 def arange(
-    start: Quantity,
-    stop: Quantity | None = None,
-    step: Quantity | None = None,
+    start: AbstractQuantity,
+    stop: AbstractQuantity | None = None,
+    step: AbstractQuantity | None = None,
     **kwargs: Any,
-) -> Quantity:
+) -> AbstractQuantity:
+    """Return evenly spaced values within a given interval.
+
+    This method is registered in the `np` and `xp` dispatchers.
+
+    Returns
+    -------
+    `unxt.AbstractQuantity`
+        Of the same type as `start`.
+
+    Examples
+    --------
+    >>> import quaxed.array_api as xp
+    >>> from unxt import Quantity
+
+    >>> xp.arange(Quantity(5, "m"))
+    Quantity['length'](Array([0, 1, 2, 3, 4], dtype=int32), unit='m')
+
+    >>> xp.arange(Quantity(5, "m"), Quantity(10, "m"))
+    Quantity['length'](Array([5, 6, 7, 8, 9], dtype=int32), unit='m')
+
+    >>> xp.arange(Quantity(5, "m"), Quantity(10, "m"), Quantity(2, "m"))
+    Quantity['length'](Array([5, 7, 9], dtype=int32), unit='m')
+
+    """
     unit = start.unit
-    return Quantity(
+    return type_np(start)(
         jax_xp.arange(
             start.value,
             stop=stop.to_units_value(unit) if stop is not None else None,
@@ -59,6 +83,19 @@ def arange(
 def empty_like(
     x: AbstractQuantity, /, *, device: Any = None, **kwargs: Any
 ) -> AbstractQuantity:
+    """Return a new array with the same shape and type as a given array.
+
+    This method is registered in the `np` and `xp` dispatchers.
+
+    Examples
+    --------
+    >>> import quaxed.array_api as xp
+    >>> from unxt import Quantity
+
+    >>> xp.empty_like(Quantity(5, "m"))
+    Quantity['length'](Array(0, dtype=int32, ...), unit='m')
+
+    """
     out = type_np(x)(jax_xp.empty_like(x.value, **kwargs), unit=x.unit)
     return jax.device_put(out, device=device)
 
@@ -67,32 +104,109 @@ def empty_like(
 
 
 @chain_dispatchers(np_dispatcher, xp_dispatcher)
-def full_like(x: AbstractQuantity, /, **kwargs: Any) -> AbstractQuantity:
+def full_like(
+    x: AbstractQuantity, /, *, fill_value: Any, **kwargs: Any
+) -> AbstractQuantity:
+    """Return a new array with the same shape and type as a given array.
+
+    This method is registered in the `np` and `xp` dispatchers.
+
+    Examples
+    --------
+    >>> import quaxed.array_api as xp
+    >>> from unxt import Quantity
+
+    >>> xp.full_like(Quantity(5, "m"), fill_value=Quantity(10, "m"))
+    Quantity['length'](Array(10, dtype=int32, ...), unit='m')
+
+    """
+    # re-dispatch to the correct implementation
     return full_like(x, fill_value, **kwargs)
+
+
+@chain_dispatchers(np_dispatcher, xp_dispatcher)  # type: ignore[no-redef]
+def full_like(
+    x: AbstractQuantity, fill_value: ArrayLike, /, **kwargs: Any
+) -> AbstractQuantity:
+    """Return a new array with the same shape and type as a given array.
+
+    This method is registered in the `np` and `xp` dispatchers.
+
+    Examples
+    --------
+    >>> import quaxed.array_api as xp
+    >>> from unxt import Quantity
+
+    >>> xp.full_like(Quantity(5, "m"), 100.0)
+    Quantity['length'](Array(100, dtype=int32, ...), unit='m')
+
+    """
+    return type_np(x)(jax_xp.full_like(x.value, fill_value, **kwargs), unit=x.unit)
 
 
 @chain_dispatchers(np_dispatcher, xp_dispatcher)  # type: ignore[no-redef]
 def full_like(
     x: AbstractQuantity, fill_value: AbstractQuantity, /, **kwargs: Any
 ) -> AbstractQuantity:
+    """Return a new array with the same shape and type as a given array.
+
+    This method is registered in the `np` and `xp` dispatchers.
+
+    Examples
+    --------
+    >>> import quaxed.array_api as xp
+    >>> from unxt import Quantity
+
+    >>> xp.full_like(Quantity(5, "m"), Quantity(10, "m"))
+    Quantity['length'](Array(10, dtype=int32, ...), unit='m')
+
+    """
     fill_val = fill_value.to_units_value(x.unit)
     return type_np(x)(jax_xp.full_like(x.value, fill_val, **kwargs), unit=x.unit)
-
-
-@chain_dispatchers(np_dispatcher, xp_dispatcher)  # type: ignore[no-redef]
-def full_like(
-    x: AbstractQuantity, fill_value: bool | int | float | complex, /, **kwargs: Any
-) -> AbstractQuantity:
-    return type_np(x)(jax_xp.full_like(x.value, fill_value, **kwargs), unit=x.unit)
 
 
 # -----------------------------------------------
 
 
 @chain_dispatchers(np_dispatcher, xp_dispatcher)
-def linspace(
-    start: Quantity, stop: Quantity, num: int | np.integer, /, **kwargs: Any
-) -> Quantity:
+def linspace(start: Quantity, stop: Quantity, num: Any, /, **kwargs: Any) -> Quantity:
+    """Return evenly spaced values within a given interval.
+
+    This method is registered in the `np` and `xp` dispatchers.
+
+    Examples
+    --------
+    >>> import quaxed.array_api as xp
+    >>> from unxt import Quantity
+
+    >>> xp.linspace(Quantity(0, "m"), Quantity(10, "m"), 5)
+    Quantity['length'](Array([ 0. ,  2.5,  5. ,  7.5, 10. ], dtype=float32), unit='m')
+
+    """
+    unit = start.unit
+    return Quantity(
+        jax_xp.linspace(
+            start.to_units_value(unit), stop.to_units_value(unit), num, **kwargs
+        ),
+        unit=unit,
+    )
+
+
+@chain_dispatchers(np_dispatcher, xp_dispatcher)
+def linspace(start: Quantity, stop: Quantity, /, **kwargs: Any) -> Quantity:
+    """Return evenly spaced values within a given interval.
+
+    This method is registered in the `np` and `xp` dispatchers.
+
+    Examples
+    --------
+    >>> import quaxed.array_api as xp
+    >>> from unxt import Quantity
+
+    >>> xp.linspace(Quantity(0, "m"), Quantity(10, "m"), num=5)
+    Quantity['length'](Array([ 0. ,  2.5,  5. ,  7.5, 10. ], dtype=float32), unit='m')
+
+    """
     unit = start.unit
     return Quantity(
         jax_xp.linspace(
@@ -102,17 +216,49 @@ def linspace(
     )
 
 
+# -----------------------------------------------
+
+
 @chain_dispatchers(np_dispatcher, xp_dispatcher)
 def ones_like(
     x: AbstractQuantity, /, *, device: Any = None, **kwargs: Any
 ) -> AbstractQuantity:
+    """Return a new array with the same shape and type as a given array.
+
+    This method is registered in the `np` and `xp` dispatchers.
+
+    Examples
+    --------
+    >>> import quaxed.array_api as xp
+    >>> from unxt import Quantity
+
+    >>> xp.ones_like(Quantity(5, "m"))
+    Quantity['length'](Array(1, dtype=int32, ...), unit='m')
+
+    """
     out = type_np(x)(jax_xp.ones_like(x.value, **kwargs), unit=x.unit)
     return jax.device_put(out, device=device)
+
+
+# -----------------------------------------------
 
 
 @chain_dispatchers(np_dispatcher, xp_dispatcher)
 def zeros_like(
     x: AbstractQuantity, /, *, device: Any = None, **kwargs: Any
 ) -> AbstractQuantity:
+    """Return a new array with the same shape and type as a given array.
+
+    This method is registered in the `np` and `xp` dispatchers.
+
+    Examples
+    --------
+    >>> import quaxed.array_api as xp
+    >>> from unxt import Quantity
+
+    >>> xp.zeros_like(Quantity(5, "m"))
+    Quantity['length'](Array(0, dtype=int32, ...), unit='m')
+
+    """
     out = type_np(x)(jax_xp.zeros_like(x.value, **kwargs), unit=x.unit)
     return jax.device_put(out, device=device)
