19323b9139ce4053148e0577e11a95c4f8eaa3c5
nstarman <nstarman@users.noreply.github.com>
2024-09-10 01:57:00 -0400

    build: bump optional_dependencies
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_interop/__init__.py
+++ src/unxt/_interop/__init__.py
@@ -6,13 +6,13 @@ __all__: list[str] = []
 from .optional_deps import OptDeps
 
 # Register interoperability
-if OptDeps.ASTROPY.is_installed:
+if OptDeps.ASTROPY.installed:
     from . import unxt_interop_astropy
 
-if OptDeps.GALA.is_installed:
+if OptDeps.GALA.installed:
     from . import unxt_interop_gala
 
-if OptDeps.MATPLOTLIB.is_installed and OptDeps.ZEROTH.is_installed:
+if OptDeps.MATPLOTLIB.installed and OptDeps.ZEROTH.installed:
     from . import unxt_interop_mpl as interop_mpl
 
     interop_mpl.setup_matplotlib_support_for_unxt(enable=True)
