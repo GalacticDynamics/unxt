1d8cccdc9378df202e2415b7c69717600a9f6626
nstarman <nstarman@users.noreply.github.com>
2024-09-15 20:50:42 -0400

    feat: compatibility with astropy 7.0
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_unxt/quantity/base_parametric.py
+++ src/unxt/_unxt/quantity/base_parametric.py
@@ -8,14 +8,14 @@ from typing import Any
 import equinox as eqx
 import jax
 import jax.core
-from astropy.units import PhysicalType, Unit, get_physical_type
+from astropy.units import PhysicalType as Dimensions, Unit
 from jaxtyping import Array, ArrayLike, Shaped
 from plum import parametric
 
 from quaxed.array_api._dispatch import dispatcher
 
 from .base import AbstractQuantity
-from unxt._unxt.dimensions.core import dimensions_of
+from unxt._unxt.dimensions.core import dimensions, dimensions_of
 from unxt._unxt.typing_ext import Unit as UnitTypes
 from unxt._unxt.units.core import units
 
@@ -36,12 +36,12 @@ class AbstractParametricQuantity(AbstractQuantity):
 
     def __post_init__(self) -> None:
         """Check whether the arguments are valid."""
-        self._type_parameter: PhysicalType
+        self._type_parameter: Dimensions
 
     def __check_init__(self) -> None:
         """Check whether the arguments are valid."""
-        expected_dimensions = self._type_parameter._physical_type_id  # noqa: SLF001
-        got_dimensions = dimensions_of(self.unit)._physical_type_id  # noqa: SLF001
+        expected_dimensions = self._type_parameter
+        got_dimensions = dimensions_of(self.unit)
         if got_dimensions != expected_dimensions:
             msg = "Physical type mismatch."  # TODO: better error message
             raise ValueError(msg)
@@ -51,42 +51,39 @@ class AbstractParametricQuantity(AbstractQuantity):
 
     @classmethod
     @dispatcher
-    def __init_type_parameter__(cls, dimensions: PhysicalType) -> tuple[PhysicalType]:
+    def __init_type_parameter__(cls, dims: Dimensions, /) -> tuple[Dimensions]:
         """Check whether the type parameters are valid."""
-        return (dimensions,)
-
-    @classmethod  # type: ignore[no-redef]
-    @dispatcher
-    def __init_type_parameter__(cls, dimensions: str) -> tuple[PhysicalType]:
-        """Check whether the type parameters are valid."""
-        try:
-            dims = get_physical_type(dimensions)
-        except ValueError:
-            dims = PhysicalType(units(dimensions), dimensions)
         return (dims,)
 
     @classmethod  # type: ignore[no-redef]
     @dispatcher
-    def __init_type_parameter__(cls, unit: UnitTypes) -> tuple[PhysicalType]:
+    def __init_type_parameter__(cls, dims: str, /) -> tuple[Dimensions]:
+        """Check whether the type parameters are valid."""
+        dims_: Dimensions
+        try:
+            dims_ = dimensions(dims)
+        except ValueError:
+            dims_ = dimensions_of(units(dims))
+        return (dims_,)
+
+    @classmethod  # type: ignore[no-redef]
+    @dispatcher
+    def __init_type_parameter__(cls, unit: UnitTypes, /) -> tuple[Dimensions]:
         """Infer the type parameter from the arguments."""
         dims = dimensions_of(unit)
         if dims != "unknown":
             return (dims,)
-        return (PhysicalType(unit, unit.to_string(fraction=False)),)
+        return (Dimensions(unit, unit.to_string(fraction=False)),)
 
     @classmethod
-    def __infer_type_parameter__(
-        cls, value: ArrayLike, unit: Any
-    ) -> tuple[PhysicalType]:
+    def __infer_type_parameter__(cls, value: ArrayLike, unit: Any) -> tuple[Dimensions]:
         """Infer the type parameter from the arguments."""
         return (dimensions_of(units(unit)),)
 
     @classmethod
     @dispatcher  # type: ignore[misc]
     def __le_type_parameter__(
-        cls,
-        left: tuple[PhysicalType],
-        right: tuple[PhysicalType],
+        cls, left: tuple[Dimensions], right: tuple[Dimensions]
     ) -> bool:
         """Define an order on type parameters.
 
@@ -99,9 +96,13 @@ class AbstractParametricQuantity(AbstractQuantity):
     def __repr__(self) -> str:
         # fmt: off
         if self._type_parameter == "unknown":
+            try:  # Astropy v6-
+                ptid = self._type_parameter._physical_type_id  # noqa: SLF001
+            except AttributeError:  # Astropy v7+
+                ptid = self._type_parameter._unit._physical_type_id  # noqa: SLF001
             dim = " ".join(
                 f"{unit}{power}" if power != 1 else unit
-                for unit, power in self._type_parameter._physical_type_id  # noqa: SLF001
+                for unit, power in ptid
             )
         else:
             dim = self._type_parameter._name_string_as_ordered_set().split("'")[1]  # noqa: SLF001
