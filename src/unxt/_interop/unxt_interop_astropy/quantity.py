6ee725b7c11ce461ec37650d7edec260f9c5b0be
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-10-22 11:28:54 -0400

    feat: move distance to coordinax (#242)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_interop/unxt_interop_astropy/quantity.py
+++ src/unxt/_interop/unxt_interop_astropy/quantity.py
@@ -14,14 +14,7 @@ from plum import conversion_method, dispatch
 import quaxed.numpy as jnp
 from dataclassish import replace
 
-from unxt import (
-    AbstractQuantity,
-    Distance,
-    DistanceModulus,
-    Parallax,
-    Quantity,
-    UncheckedQuantity,
-)
+from unxt import AbstractQuantity, Quantity, UncheckedQuantity
 from unxt._interop.optional_deps import OptDeps
 
 # ============================================================================
@@ -172,69 +165,6 @@ def convert_astropy_quantity_to_unxt_uncheckedquantity(
 
     """
     return UncheckedQuantity(q.value, q.unit)
-
-
-# ============================================================================
-# Distance
-
-
-@conversion_method(type_from=AstropyQuantity, type_to=Distance)  # type: ignore[misc]
-def convert_astropy_quantity_to_unxt_distance(q: AstropyQuantity, /) -> Distance:
-    """Convert a `astropy.units.Quantity` to a `unxt.Distance`.
-
-    Examples
-    --------
-    >>> from astropy.units import Quantity as AstropyQuantity
-    >>> from plum import convert
-    >>> from unxt import Distance
-
-    >>> convert(AstropyQuantity(1.0, "cm"), Distance)
-    Distance(Array(1., dtype=float32), unit='cm')
-
-    """
-    return Distance(q.value, q.unit)
-
-
-# ============================================================================
-# Parallax
-
-
-@conversion_method(type_from=AstropyQuantity, type_to=Parallax)  # type: ignore[misc]
-def convert_astropy_quantity_to_unxt_parallax(q: AstropyQuantity, /) -> Parallax:
-    """Convert a `astropy.units.Quantity` to a `unxt.Parallax`.
-
-    Examples
-    --------
-    >>> from astropy.units import Quantity as AstropyQuantity
-    >>> from plum import convert
-    >>> from unxt import Parallax
-
-    >>> convert(AstropyQuantity(1.0, "radian"), Parallax)
-    Parallax(Array(1., dtype=float32), unit='rad')
-
-    """
-    return Parallax(q.value, q.unit)
-
-
-# ============================================================================
-# Distance Modulus
-
-
-@conversion_method(type_from=AstropyQuantity, type_to=DistanceModulus)  # type: ignore[misc]
-def convert_astropy_quantity_to_unxt_distmod(q: AstropyQuantity, /) -> DistanceModulus:
-    """Convert a `astropy.units.Quantity` to a `unxt.DistanceModulus`.
-
-    Examples
-    --------
-    >>> from astropy.units import Quantity as AstropyQuantity
-    >>> from plum import convert
-    >>> from unxt import DistanceModulus
-
-    >>> convert(AstropyQuantity(1.0, "mag"), DistanceModulus)
-    DistanceModulus(Array(1., dtype=float32), unit='mag')
-
-    """
-    return DistanceModulus(q.value, q.unit)
 
 
 ###############################################################################
