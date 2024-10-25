05d0301d64f892ae52650724964300f224027327
nstarman <nstarman@users.noreply.github.com>
2024-10-25 11:38:38 -0400

    refactor: use abstracts for dimensions
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_src/dimensions/core.py
+++ src/unxt/_src/dimensions/core.py
@@ -5,7 +5,7 @@ Copyright (c) 2023 Galactic Dynamics. All rights reserved.
 
 __all__ = ["dimensions", "dimensions_of"]
 
-from typing import TypeAlias
+from typing import Any, TypeAlias
 
 import astropy.units as u
 from astropy.units import Unit
@@ -17,6 +17,20 @@ AbstractDimensions: TypeAlias = u.PhysicalType
 
 # ===================================================================
 # Construct the dimensions
+
+
+@dispatch.abstract
+def dimensions(obj: Any, /) -> AbstractDimensions:
+    """Return the dimensions of the given units.
+
+    .. note::
+
+        This function uses multiple dispatch. Dispatches made in other modules
+        may not be included in the rendered docs. To see the full range of
+        options, execute ``unxt.dimensions.methods`` in an interactive Python
+        session.
+
+    """
 
 
 @dispatch
@@ -57,7 +71,21 @@ def dimensions(obj: str, /) -> AbstractDimensions:
 # Get the dimensions
 
 
-@dispatch  # type: ignore[misc]
+@dispatch.abstract
+def dimensions_of(obj: Any, /) -> AbstractDimensions:
+    """Return the dimensions of the given units.
+
+    .. note::
+
+        This function uses multiple dispatch. Dispatches made in other modules
+        may not be included in the rendered docs. To see the full range of
+        options, execute ``unxt.dimensions_of.methods`` in an interactive Python
+        session.
+
+    """
+
+
+@dispatch
 def dimensions_of(obj: AbstractDimensions, /) -> AbstractDimensions:
     """Return the dimensions of the given units.
 
