ea090ccc4ac18d873c28727087da109a077d128d
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-10-29 08:33:40 -0400

    ci: stop skipping windows (#261)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 """Doctest configuration."""
 
 import os
-import platform
 from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
 from typing import Any
 
@@ -10,20 +9,12 @@ from sybil.parsers.rest import DocTestParser, PythonCodeBlockParser, SkipParser
 
 from optional_dependencies import OptionalDependencyEnum, auto
 
-# TODO: stop skipping doctests on Windows when there is uniform support for
-#       numpy 2.0+ scalar repr. On windows it is printed as 1.0 instead of
-#       `np.float64(1.0)`.
-parsers = (
-    [DocTestParser(optionflags=ELLIPSIS | NORMALIZE_WHITESPACE)]
-    if platform.system() != "Windows"
-    else []
-) + [
-    PythonCodeBlockParser(),
-    SkipParser(),
-]
-
 pytest_collect_file = Sybil(
-    parsers=parsers,
+    parsers=[
+        DocTestParser(optionflags=ELLIPSIS | NORMALIZE_WHITESPACE),
+        PythonCodeBlockParser(),
+        SkipParser(),
+    ],
     patterns=["*.rst", "*.py"],
 ).pytest()
 
