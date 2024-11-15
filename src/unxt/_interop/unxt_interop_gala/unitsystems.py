d1d5831741af67784e4c46cb1e5660fe17326a17
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-11-15 00:43:27 -0500

    feat: register converters for gala unitsystems (#283)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_interop/unxt_interop_gala/unitsystems.py
+++ src/unxt/_interop/unxt_interop_gala/unitsystems.py
@@ -3,11 +3,14 @@
 
 __all__: list[str] = []
 
+from typing import Any
+
+from astropy.units import UnitBase as AstropyUnit
 from gala.units import (  # pylint: disable=import-error
     DimensionlessUnitSystem as GalaDimensionlessUnitSystem,
     UnitSystem as GalaUnitSystem,
 )
-from plum import dispatch
+from plum import conversion_method, convert, dispatch
 
 from unxt.unitsystems import AbstractUnitSystem, DimensionlessUnitSystem, dimensionless
 
@@ -48,3 +51,58 @@ def unitsystem(_: GalaDimensionlessUnitSystem, /) -> DimensionlessUnitSystem:
 
     """  # noqa: E501
     return dimensionless
+
+
+# =============================================================================
+# Convert
+
+
+@conversion_method(type_from=GalaUnitSystem, type_to=AbstractUnitSystem)  # type: ignore[misc]
+def convert_gala_unitsystem_to_unxt_unitsystem(
+    usys: GalaUnitSystem, /
+) -> AbstractUnitSystem:
+    """Convert a `gala.units.UnitSystem` to a `unxt.AbstractUnitSystem`.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> import gala.units as gu
+    >>> import astropy.units as apyu
+
+    >>> usys = gu.UnitSystem(apyu.km, apyu.s, apyu.Msun, apyu.radian)
+    >>> usys
+    <UnitSystem (km, s, solMass, rad)>
+
+    >>> convert(usys, u.AbstractUnitSystem)
+    unitsystem(km, s, solMass, rad)
+
+    """
+    return unitsystem(usys)
+
+
+@conversion_method(type_from=AbstractUnitSystem, type_to=GalaUnitSystem)  # type: ignore[misc]
+def convert_unxt_unitsystem_to_gala_unitsystem(
+    usys: AbstractUnitSystem, /
+) -> GalaUnitSystem:
+    """Convert a `unxt.AbstractUnitSystem` to a `gala.units.UnitSystem`.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> import gala.units as gu
+    >>> import astropy.units as apyu
+    >>> from plum import convert
+
+    >>> usys = u.unitsystem(apyu.km, apyu.s, apyu.Msun, apyu.radian)
+    >>> usys
+    unitsystem(km, s, solMass, rad)
+
+    >>> convert(usys, gu.UnitSystem)
+    <UnitSystem (km, s, solMass, rad)>
+
+    """
+
+    def convert_unit_to_apyu(u: Any) -> AstropyUnit:
+        return convert(u, AstropyUnit)
+
+    return GalaUnitSystem(*map(convert_unit_to_apyu, usys.base_units))
