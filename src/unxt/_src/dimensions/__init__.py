833dc6e04156cfba816da24254ebc2bb3a7732be
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-21 14:54:22 -0500

    Backport PR #372: 🎨 style(dimensions): restructure private files

--- src/unxt/_src/dimensions/__init__.py
+++ src/unxt/_src/dimensions/__init__.py
@@ -6,4 +6,8 @@ This is the private implementation of the dimensions module.
 
 __all__ = ["AbstractDimension", "dimension", "dimension_of"]
 
-from .core import AbstractDimension, dimension, dimension_of
+from .api import AbstractDimension, dimension, dimension_of
+
+# Register the dispatches
+# isort: split
+from . import core  # noqa: F401
