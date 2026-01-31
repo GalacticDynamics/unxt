72bbe5f05c09301b0002ee4aac24a0de76ec5132
Nathaniel Starkman <nstarman@users.noreply.github.com>
2026-01-30 23:32:11 -0500

    🐛 fix(unxt): dataclassish interop (#577)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/unxt/_interop/unxt_interop_astropy/dimensions.py
+++ src/unxt/_interop/unxt_interop_astropy/dimensions.py
@@ -1,15 +1,17 @@
-"""Unitsystem compatibility."""
+"""Dimensions compatibility."""
 
 __all__: tuple[str, ...] = ()
 
 
+from dataclasses import MISSING, Field
+
 import astropy.units as apyu
-from plum import dispatch
+import plum
 
 from unxt.dims import AbstractDimension
 
 
-@dispatch
+@plum.dispatch
 def dimension_of(obj: apyu.Quantity, /) -> AbstractDimension:
     """Return the dimension of a quantity.
 
@@ -24,3 +26,28 @@ def dimension_of(obj: apyu.Quantity, /) -> AbstractDimension:
 
     """
     return dimension_of(obj.unit)
+
+
+# ===================================================
+# `Dataclassish` support
+
+
+@plum.dispatch
+def fields(obj: AbstractDimension, /) -> tuple[Field, ...]:
+    """Return the fields of a dimension.
+
+    Examples
+    --------
+    >>> import dataclassish as dc
+    >>> import astropy.units as apyu
+
+    >>> dim = apyu.get_physical_type("length")
+    >>> dc.fields(dim)
+    (Field(name='_unit',...), Field(name='_physical_type',...))
+
+    """
+    unit_field = Field(MISSING, MISSING, True, False, False, False, {}, False)  # noqa: FBT003
+    unit_field.name = "_unit"
+    physical_type_field = Field(MISSING, MISSING, True, False, False, False, {}, False)  # noqa: FBT003
+    physical_type_field.name = "_physical_type"
+    return (unit_field, physical_type_field)
