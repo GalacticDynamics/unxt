ece68cac86fdff7d4781f2dab11961bbbb3b4263
nstarman <nstarman@users.noreply.github.com>
2026-04-20 23:03:48 -0400

    🎨 style: clean up private imports

--- src/unxt/_src/unitsystems/__init__.py
+++ src/unxt/_src/unitsystems/__init__.py
@@ -1,40 +1,8 @@
 """Tools for representing systems of units using ``astropy.units``."""
-# pylint: disable=duplicate-code
 
-__all__ = (
-    "unitsystem",
-    "unitsystem_of",
-    # classes
-    "AbstractUnitSystem",
-    "UNITSYSTEMS_REGISTRY",
-    "DimensionlessUnitSystem",
-    "LTMAUnitSystem",
-    # unit system instance
-    "galactic",
-    "dimensionless",
-    "solarsystem",
-    "si",
-    "cgs",
-    # unit system alias
-    "NAMED_UNIT_SYSTEMS",
-    # unit system flags
-    "AbstractUSysFlag",
-    "StandardUSysFlag",
-    "DynamicalSimUSysFlag",
-    # functions
-    "equivalent",
-)
-
-from .base import UNITSYSTEMS_REGISTRY, AbstractUnitSystem
-from .builtin import DimensionlessUnitSystem, LTMAUnitSystem
-from .compare import equivalent
-from .core import unitsystem, unitsystem_of
-from .flags import AbstractUSysFlag, DynamicalSimUSysFlag, StandardUSysFlag
-from .realizations import (
-    NAMED_UNIT_SYSTEMS,
-    cgs,
-    dimensionless,
-    galactic,
-    si,
-    solarsystem,
-)
+from .base import *
+from .builtin import *
+from .compare import *
+from .core import *
+from .flags import *
+from .realizations import *
